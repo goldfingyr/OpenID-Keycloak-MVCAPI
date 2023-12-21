86bd44b7d84d117f44bf1d70477a4c43d2bd4b48
Karsten Jeppesen <kaje@ucn.dk>
2023-12-21 09:23:19 +0100

    V2.0 - Working
    VS2022

--- OpenID-Keycloak-MVCAPI/OpenID-Keycloak-MVCAPI/Controllers/WeatherForecastController.cs
+++ OpenID-Keycloak-MVCAPI/OpenID-Keycloak-MVCAPI/Controllers/WeatherForecastController.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace OpenID_Keycloak_MVCAPI.Controllers
@@ -18,9 +20,13 @@ namespace OpenID_Keycloak_MVCAPI.Controllers
             _logger = logger;
         }
 
+        //[Authorize(Roles = "admin")]                  // Uncomment for Resource Owner Password Flow
+        //[Authorize]                                   // Uncomment for Client Credentials Flow
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            var request = HttpContext.Request.Headers;
+            string accessToken = HttpContext.GetTokenAsync("access_token").Result;
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
--- OpenID-Keycloak-MVCAPI/OpenID-Keycloak-MVCAPI/Program.cs
+++ OpenID-Keycloak-MVCAPI/OpenID-Keycloak-MVCAPI/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,58 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// >>> Add Swagger Auth option
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "My API",
+        Version = "v1"
+    });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please insert JWT with Bearer into field",
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+   {
+     new OpenApiSecurityScheme
+     {
+       Reference = new OpenApiReference
+       {
+         Type = ReferenceType.SecurityScheme,
+         Id = "Bearer"
+       }
+      },
+      new string[] { }
+    }
+  });
+});
+// <<<
+
+// >>> Add authorization
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(opts =>
+    {
+        opts.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = false,
+            ValidAudience = "account",  // Debug accesstoken using jwt.io to get the value of "aud": "account",
+            ValidIssuers = new []{ System.Environment.GetEnvironmentVariable("OpenIDRealmURI") },
+        };
+        opts.Authority = System.Environment.GetEnvironmentVariable("OpenIDRealmURI");
+        opts.Audience = System.Environment.GetEnvironmentVariable("OpenIDRealm");
+    });
+
+builder.Services.AddAuthorization();
+// <<<
+
 
 var app = builder.Build();
 
@@ -18,6 +73,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
